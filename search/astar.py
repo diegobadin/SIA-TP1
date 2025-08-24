afb700b44a195be985cb5da32beb31ee62ea8974
valentinamartireta <vmartireta@itba.edu.ar>
2025-08-23 21:36:36 -0300

    commit informed searches

--- search/astar.py
+++ search/astar.py
@@ -1,9 +1,6 @@
 from search import informed_search
 
 
-def a_star_priority(state, heuristic, g_val):
-    return g_val + heuristic(state, state.goal_positions)
-
-
 def solve_with_astar(initial_state, heuristic, goal_positions, board):
+    a_star_priority = lambda state, goals, g_val: g_val + heuristic(state, goals)
     return informed_search.solve_informed_search(initial_state, a_star_priority, goal_positions)
--- search/greedy.py
+++ search/greedy.py
@@ -1,9 +1,6 @@
 from search import informed_search
 
 
-def greedy_priority(state, heuristic, g_val):
-    return heuristic(state, state.goal_positions)
-
-
 def solve_with_greedy(initial_state, heuristic, goal_positions, board):
+    greedy_priority = lambda state, goals, g_val: heuristic(state, goals)
     return informed_search.solve_informed_search(initial_state, greedy_priority, goal_positions)
