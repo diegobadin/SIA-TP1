898982bb5e63b211cabc2fe69d27b2c14492b970
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2025-08-26 19:31:25 -0300

    deadlocks added as heuristic

--- search/greedy.py
+++ search/greedy.py
@@ -2,5 +2,5 @@ from search import informed_search
 
 
 def solve_with_greedy(initial_state, heuristic, goal_positions, board):
-    greedy_priority = lambda state, goals, g_val: heuristic(state, goals)
+    greedy_priority = lambda state, goals, g_val, walls: heuristic(state, goals, walls)
     return informed_search.solve_informed_search(initial_state, greedy_priority, goal_positions, board)
--- state.py
+++ state.py
@@ -25,11 +25,12 @@ class State:
     def __init__(self, player, boxes):
         self.player = player
         self.boxes = frozenset(boxes)  # frozenset so states are hashable
+        self._hash = hash((self.player, self.boxes))
     def __eq__(self, other):
         return self.player == other.player and self.boxes == other.boxes
 
     def __hash__(self):
-        return hash((self.player, self.boxes))
+        return self._hash
 
     def __repr__(self):
         return f"State(player={self.player}, boxes={set(self.boxes)})"
@@ -39,54 +40,6 @@ class State:
         Returns True if all boxes are on the goal positions, False otherwise.
         """
         return self.boxes == goal_positions
-
-
-    def is_deadlock(self, new_boxes,new_box_pos, walls, goal_positions):
-        row, col = new_box_pos
-        # draw_sokoban(walls, self.boxes, goal_positions, self.player)
-
-
-        # pares de direcciones que forman esquinas
-        corner_checks = [
-            (directions['U'], directions['R']),
-            (directions['R'], directions['D']),
-            (directions['D'], directions['L']),
-            (directions['L'], directions['U'])
-        ]
-
-        for d1, d2 in corner_checks:
-            r1, c1 = row + d1[0], col + d1[1]
-            r2, c2 = row + d2[0], col + d2[1]
-            if (r1, c1) in walls and (r2, c2) in walls:
-                return True
-
-        for key, (di, dj) in directions.items():
-            neighbor = (row + di, col + dj)
-            if neighbor in new_boxes:
-                for offset1, offset2 in l_checks[key]:
-                    r1, c1 = row + offset1[0], col + offset1[1]
-                    r2, c2 = row + offset2[0], col + offset2[1]
-                    if (r1, c1) in walls and (r2, c2) in walls:
-                        return True
-
-        for key, (di, dj) in directions.items():
-            neighbor = (row + di, col + dj)
-
-            if neighbor in walls:
-                if key in ["U", "D"]:
-                    wall_row = [(row + di, c) for c in range(min(w[1] for w in walls), max(w[1] for w in walls) + 1)]
-                    same_row = [(row, c) for c in range(min(w[1] for w in walls), max(w[1] for w in walls) + 1)]
-                    if not any((row, c) in goal_positions for (_, c) in same_row) and all(
-                            pos in walls for pos in wall_row):
-                        return True
-
-                if key in ["L", "R"]:
-                    wall_col = [(r, col + dj) for r in range(min(w[0] for w in walls), max(w[0] for w in walls) + 1)]
-                    same_col = [(r, col) for r in range(min(w[0] for w in walls), max(w[0] for w in walls) + 1)]
-                    if not any((r, col) in goal_positions for (r, _) in same_col) and all(
-                            pos in walls for pos in wall_col):
-                        return True
-        return False
 
 
     def get_possible_moves(self,walls,goal_positions):
@@ -105,8 +58,7 @@ class State:
                     continue
 
                 new_boxes = frozenset(self.boxes - {new_player} | {new_box_pos})
-                if not self.is_deadlock(new_boxes,new_box_pos,walls,goal_positions) or new_box_pos in goal_positions:
-                    moves.append((action, State(new_player, new_boxes)))
+                moves.append((action, State(new_player, new_boxes)))
             else:
                 moves.append((action, State(new_player, self.boxes)))
 
